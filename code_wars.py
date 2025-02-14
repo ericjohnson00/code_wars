576871d5fe8fb1390c626db44d204e59440e9ce7
Eric Johnson <ericjohnson8055@gmail.com>
2025-02-14 02:11:56 -0600

    code_wars

--- code_wars.py
+++ code_wars.py
@@ -138,3 +138,41 @@ def boolean_to_string(b):
     
 print(boolean_to_string(True))
 
+
+
+
+# Grasshopper - Summation
+# Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+# For example (Input -> Output):
+
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+def summation(num):
+    return sum(range(num + 1))
+
+print(summation(8))
+
+
+
+# Sort the odd
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+# Examples
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
+def sort_array(source_array):
+    odds = sorted((x for x in source_array if x % 2 != 0), reverse=True)
+    return [x if x % 2 == 0 else odds.pop() for x in source_array]
+
+print(sort_array([5, 3, 2, 8, 1, 4]))
+
+# another example
+# def sort_array(source_array):
+#     odds = sorted((x for x in source_array if x % 2 != 0), reverse=True)
+#     return [x if x % 2 == 0 else odds.pop(0) for x in source_array]
+
